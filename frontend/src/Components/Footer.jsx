360a2e3bb1d43fa7d8310b3cf938bb2d3c4ef50a
Chenu <piyushbagadi51@gmail.com>
2025-02-18 19:09:30 +0530

    login page done

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='fixed bottom-0 bg-gray-400 w-full '>
-            <footer className='text-center p-2' >Copy @2025</footer>
+        <div className='relative bottom-0 mt-20  text-gray-400 font-medium w-full '>
+            <footer className='text-center p-2' >© 2021. - 2025 All Rights Reserved. PuAlumini</footer>
         </div>
     )
 }
